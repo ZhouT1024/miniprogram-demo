b666ba4fac08fa7b918d14aacfc7838eacd86107
zhoutao <immrtao@163.com>
2023-07-21 01:15:10 +0800

    获取小程序版本号

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -1,1 +1,15 @@
-Page({})
+Page({
+
+  onLoad() {
+    console.log('loading...')
+  },
+
+  /**
+   * 导航到一个页面
+   */
+  navigateTo(event: any) {
+    console.log('function navigateTo():', event)
+    const { url } = event.currentTarget.dataset
+    wx.navigateTo({ url })
+  },
+})
